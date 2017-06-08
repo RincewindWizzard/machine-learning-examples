31ea7b43a108dec8d2c374f6c1ece5471b6fe51c
RincewindWizzard <git@magierdinge.de>
2017-06-08 14:49:32 +0200

    testranges

--- scikit_example.py
+++ scikit_example.py
@@ -1,22 +1,29 @@
+import math
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from scipy import misc
 from sklearn import svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+ 
 
 
 def training_data(universe, n = 10):
   width, height = universe.shape
   if n > width*height: raise ValueError("Not enough input space for sample size!")
 
-  data = [[x,y,universe[x][y]] for x in range(width) for y in range(height)]
+  data = [[x, y, universe[x][y]] for x in range(width) for y in range(height)]
   data = random.sample(data, n)
 
   return data
 
+def normalize_data(data, width, height):
+  return [ [d[0]/width, d[1]/height] + d[2:] for d in data]
+
 def difference_image(imga, imgb):
-  if not imga.shape == imgb.shape: raise ValueError("Th two images have unequal shapes!")
+  if not imga.shape == imgb.shape: raise ValueError("The two images have unequal shapes!")
 
   errors = 0
   diff = []
@@ -29,11 +36,13 @@ def difference_image(imga, imgb):
       row.append(int(not correct))
   return np.array(diff), errors
 
-def showFitGraph(original, prediction, training_data=None):
+def showFitGraph(original, *predictions, training_data=None):
   width, height = original.shape
 
-  f, ax = plt.subplots(1, 2)
-  left, right = ax
+  count = 1 + len(predictions)
+  rowsize = 6
+  f, ax = plt.subplots(math.ceil(count // rowsize), min(count, rowsize))
+  left = ax[0][0]
   left.set_title('Training data')
   
 
@@ -51,49 +60,93 @@ def showFitGraph(original, prediction, training_data=None):
     )
   left.axis([0, width-1, 0, height-1])
 
+  for i, (title, prediction) in enumerate(predictions):
+    graph = ax[(i + 1) // rowsize][(i + 1) % rowsize]
+    graph.imshow(prediction, cmap='Greys')
+    graph.axis([0, width-1, 0, height-1])
 
-  right.imshow(prediction, cmap='Greys')
-  right.axis([0, width-1, 0, height-1])
 
+    cmap = plt.cm.Wistia
+    # Get the colormap colors
+    my_cmap = cmap(np.arange(cmap.N))
 
-  cmap = plt.cm.Wistia
-  # Get the colormap colors
-  my_cmap = cmap(np.arange(cmap.N))
+    # Set alpha
+    my_cmap[:,-1] = np.linspace(0, 0.5, cmap.N)
 
-  # Set alpha
-  my_cmap[:,-1] = np.linspace(0, 0.5, cmap.N)
-
-  # Create new colormap
-  my_cmap = ListedColormap(my_cmap)
-  
-  diffimg, errors = difference_image(original, prediction)
-  right.set_title('Prediction ({:0.2f} % accurancy)'.format(100 - 100* errors / (width*height)))
-  right.imshow(diffimg, cmap=my_cmap)
+    # Create new colormap
+    my_cmap = ListedColormap(my_cmap)
+    
+    diffimg, errors = difference_image(original, prediction)
+    graph.set_title('{} ({:0.2f} % accurancy)'.format(title, 100 - 100* errors / (width*height)))
+    #right.imshow(diffimg, cmap=my_cmap)
 
   plt.show()
 
-def learn(data):
+
+
+def learnSVM(data):
   inputs  = [ x[:2] for x in data ]
   targets = [ x[2]  for x in data ]
   clf = svm.SVC(gamma=0.001, C=100.)
   clf.fit(inputs, targets)
   return clf.predict
 
+def learnMLP(hidden_layer_sizes=(4)):
+  def learn(data):
+    inputs  = [ x[:2] for x in data ]
+    targets = [ int(bool(x[2]))  for x in data ]
 
-def test(image, learn, sample_size=1000):
-  data = training_data(image, sample_size)
+    scaler = StandardScaler()  
+    # Don't cheat - fit only on training data
+    scaler.fit(inputs)  
+    data = scaler.transform(inputs)  
+
+
+    clf = MLPClassifier(
+      solver='lbfgs', 
+      alpha=1e-5, 
+      hidden_layer_sizes=hidden_layer_sizes, 
+      random_state=1
+    )
+    clf.fit(inputs, targets)
+
+    def predict(test_data):
+      # apply same transformation to test data
+      test_data = scaler.transform(test_data)
+      return clf.predict(test_data)
+    return predict
+  return learn
+
+def test(image, learn, data, normalize=False):
+  if normalize:
+    data = normalize_data(data, *image.shape)
+
   predict = learn(data)
 
-  xs = np.arange(0, image.shape[0], 1)
-  ys = np.arange(0, image.shape[1], 1)
-
+  if normalize:
+    xs = np.arange(0, 1, 1/image.shape[0])
+    ys = np.arange(0, 1, 1/image.shape[1])
+  else:
+    xs = np.arange(0, image.shape[0], 1)
+    ys = np.arange(0, image.shape[1], 1)
 
   prediction = predict([[x, y] for x in xs for y in ys]).reshape(image.shape)
-  showFitGraph(image, prediction, data)
+  return prediction
 
 def main():
   image = misc.imread('datasets/bezier_diagonal_2.png')
-  test(image, learn, sample_size=500)
+  data = training_data(image, 1000)
 
+  images = [('SVM', test(image, learnSVM, data))]
+  for i in range(16):
+    images.append((
+      'MLP', 
+      test(
+        image, 
+        learnMLP(
+          hidden_layer_sizes = (10*i+2, 10*i+2, 10*i+2, 10*i+2,)
+      ), data, normalize=True)))
+
+  showFitGraph(image, *images, training_data=data)
 if __name__ == '__main__':
   main()
